83bd514c3e03334563881abfffcff8e5063b4c13
Pi làm nè <wanbicoi123@gmail.com>
2023-08-09 10:28:47 +0700

    fix: build

--- src/common/dump-data/index.ts
+++ src/common/dump-data/index.ts
@@ -1,29 +1,28 @@
 import { IDoctor, IService } from "@/common/types";
 
-export const DOCTORS: IDoctor[] = [
-  {
-    id: "123123",
-    key: 1,
-    image:
-      "https://ucarecdn.com/0a90cce9-2606-4bc3-a89b-105a0334e821/-/quality/smart/-/format/auto/",
-    name: "Nguyễn Thị Thuý Linh",
-    experience_year: 7,
-    desc_doctor:
-      "Lorem ipsum dolor sit amet consectetur. Diam cursus amet ultricies mauris enim at velit. Dignissim ultricies pretium purus egestas ut elementum.",
-    major: "Răng hàm mặt",
-    service_id: "askldfj",
-  },
-];
+// export const DOCTORS: IDoctor[] = [
+//   {
+//     id: "123123",
+//     key: 1,
+//     image:
+//       "https://ucarecdn.com/0a90cce9-2606-4bc3-a89b-105a0334e821/-/quality/smart/-/format/auto/",
+//     name: "Nguyễn Thị Thuý Linh",
+//     experience_year: 7,
+//     desc_doctor:
+//       "Lorem ipsum dolor sit amet consectetur. Diam cursus amet ultricies mauris enim at velit. Dignissim ultricies pretium purus egestas ut elementum.",
+//     major: "Răng hàm mặt",
+//     service_id: "askldfj",
+//   },
+// ];
+//
+// export const SERVICE: IService = {
+//   image:
+//     "https://ucarecdn.com/0a90cce9-2606-4bc3-a89b-105a0334e821/-/quality/smart/-/format/auto/",
+//   price: 0,
+//   name: "Tên chi tiết dịch vụ",
+//   category_id: "lkajsdflkj",
+//   id: "",
+//   key: 0,
+// };
 
-export const SERVICE: IService = {
-  image:
-    "https://ucarecdn.com/0a90cce9-2606-4bc3-a89b-105a0334e821/-/quality/smart/-/format/auto/",
-  price: 0,
-  content: "dd/mm/yy",
-  name: "Tên chi tiết dịch vụ",
-  category_id: "lkajsdflkj",
-  id: "",
-  key: 0,
-};
-
-export const SERVICES: IService[] = [1, 2, 3, 4, 5, 6, 7, 8].map(() => SERVICE);
+// export const SERVICES: IService[] = [1, 2, 3, 4, 5, 6, 7, 8].map(() => SERVICE);
